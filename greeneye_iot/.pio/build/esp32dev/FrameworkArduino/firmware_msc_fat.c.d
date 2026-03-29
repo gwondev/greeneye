4924a663d86b3ac8eecf3a182a983b4bb0e3fd39
gwon <gwondev0323@gmail.com>
2026-03-29 18:04:24 +0900

    change

---
+++ greeneye_iot/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/gwon/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/gwon/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
